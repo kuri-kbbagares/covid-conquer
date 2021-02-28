48a6943339a98e6f5ba5221d6ba0732455ff5b9c
shaina-espulgar <72699933+shaina-espulgar@users.noreply.github.com>
2021-03-01 05:47:02 +0800

    Minor changes

--- src/PlayMenu.lua
+++ src/PlayMenu.lua
@@ -22,9 +22,11 @@ end
 function PlayMenu:render()
     Player.xdelt = 0
     Player.ydelt = 0
-    love.graphics.rectangle('fill', VIRTUAL_WIDTH * 0.20, VIRTUAL_HEIGHT * 0.10, 300, 300)
+    
 
     if virusDamage < DEATH then
+        love.graphics.setColor(128/255, 0/255, 0/255, 1)
+        love.graphics.rectangle('fill', VIRTUAL_WIDTH * 0.345, VIRTUAL_HEIGHT * 0.15, 300, 350)
         love.graphics.setFont(gFonts['largeFont'])
         love.graphics.setColor(self.clickScript[3].textcolor)
         love.graphics.printf('Game Paused', 0, VIRTUAL_HEIGHT * 0.2, VIRTUAL_WIDTH, 'center')
@@ -41,11 +43,13 @@ function PlayMenu:render()
 
     else
         gSounds['on-death']:play()
+        love.graphics.setColor(128/255, 0/255, 0/255, 1)
+        love.graphics.rectangle('fill', VIRTUAL_WIDTH * 0.24, VIRTUAL_HEIGHT * 0.15, 500, 370)
         love.graphics.setFont(gFonts['medium_titleFont'])
         love.graphics.setColor(1,0,0,1)
         love.graphics.printf('GAME OVER', 0, VIRTUAL_HEIGHT * 0.2, VIRTUAL_WIDTH, 'center')
 
-        love.graphics.setFont(gFonts['mediumFont'])
+        love.graphics.setFont(gFonts['largeFont'])
         love.graphics.setColor(1,1,1,1)
         love.graphics.printf('Score: ' .. Scoring.score, 0, VIRTUAL_HEIGHT * 0.4, VIRTUAL_WIDTH, 'center')
         love.graphics.printf('Viruses Killed: ' .. Scoring.virus, 0, VIRTUAL_HEIGHT * 0.5, VIRTUAL_WIDTH, 'center')
